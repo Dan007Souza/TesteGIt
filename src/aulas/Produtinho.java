8750ab17b279dacef78cd1868ce124f2fe1daa98
Daniel <danielbrsouza02@gmail.com>
2023-08-17 16:48:50 -0300

    segundo commit

---
+++ src/aulas/Produtinho.java
@@ -0,0 +1,15 @@
+package aulas;
+
+public class Produtinho {
+
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("Eu vou ser o rei dos piratas.");
+		
+		System.out.println("Stay with me");
+
+	}
+
+}
--- src/aulas/Teste.java
+++ src/aulas/Teste.java
@@ -7,6 +7,9 @@ public class Teste {
 	public static void main(String[] args) {
 	
 	System.out.println("Hello world");
+	
+	System.out.println("Primeira modificação");
+	
 		
 	}
 }
